dab812baef3f29f842844d234d61f310f54fdbbe
Lucas Coutinho <luca.coutinho1@gmail.com>
2021-11-04 08:50:51 -0300

    fix: corrige nomes dos testes

--- tests/Feature/Temporada/DestroyTest.php
+++ tests/Feature/Temporada/DestroyTest.php
@@ -15,7 +15,7 @@ class DestroyTest extends TestCase
     private const ROTA  = 'serie.temporadas.destroy';
     private const GUARD = 'api';
 
-    public function test_pode_excluir_estudios()
+    public function test_pode_excluir_temporadas()
     {
         $serie = Serie::factory()->create();
         $temporada = Temporada::factory()->create();
@@ -30,7 +30,7 @@ class DestroyTest extends TestCase
         $response->assertJson([])->assertStatus(200);
     }
 
-    public function test_nao_pode_excluir_estudios_sem_autenticar()
+    public function test_nao_pode_excluir_temporadas_sem_autenticar()
     {
         $serie = Serie::factory()->create();
         $temporada = Temporada::factory()->create();
@@ -42,7 +42,7 @@ class DestroyTest extends TestCase
         ])->assertStatus(401);
     }
 
-    public function test_nao_pode_excluir_estudios_sem_permissao()
+    public function test_nao_pode_excluir_temporadas_sem_permissao()
     {
         $serie = Serie::factory()->create();
         $temporada = Temporada::factory()->create();
